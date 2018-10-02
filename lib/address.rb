7c988efe844f5b653bc40c506689e961d24c749c
berkeleycole <alyssamhope@gmail.com>
2018-10-02 10:09:08 -0700

    sets up controller, geocoding logic and routing

--- lib/address.rb
+++ lib/address.rb
@@ -2,4 +2,49 @@ require_relative 'geocoding'
 
 class Address
   attr_accessor :lat, :lng, :full_address
+
+  def reverse_geocode(lat, lng)
+	  results = Geocoder.search([lat, lng])
+	  results.first.address
+  end
+
+  def reverse_geocoded?
+    if @lat and @lng
+      true
+    else
+      false
+    end
+  end
+
+  def geocode(full_address)
+
+    @full_address = full_address
+
+	  results = Geocoder.search(@full_address)
+
+    return false unless results
+
+    # points = results.first
+    #
+    # @lat = points[0]
+    # @lng = points[1]
+
+    results
+  end
+
+  def geocoded?
+    if @lat && @lng
+      true
+    else
+      false
+    end
+  end
+
+  def coordinates
+    [@lat, @lng]
+  end
+
+  def miles_to(to)
+    Geocoder::Calculations.distance_between(@full_address || coordinates, to)
+  end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,66 @@
 require 'sinatra/base'
-Dir['./lib/*.rb'].each { |f| require f }
+require 'sinatra/activerecord'
+# hot reloading for sinatra
+require 'sinatra/reloader'
+
+# requires anything in the lib folder
+Dir['./lib/*.rb', './models/*.rb'].each { |f| require f }
 
 class Main < Sinatra::Base
+  before do
+    new_address
+  end
+
   get '/' do
-    erb :index #, locals: { address: address }
+    locations = Location.all
+    p locations.to_json
+    erb :index, :locals => { :locations => locations }
+  end
+
+  post '/geocode' do
+    # guard statement -- needs an error
+    return unless params['street'].present? and params['city'].present? and params['state'].present? and params['country'].present?
+
+    coords = @address.reverse_geocode(params['street'], params['city'], params['state'], params['country'])
+
+    location = Location.new(
+      lat: coords[0],
+      lng: coords[1],
+      full_address: parse_full_address(params['street'], params['city'], params['state'], params['country'])
+    )
+
+    if location.save
+      redirect '/'
+    else
+      redirect '/error'
+    end
+  end
+
+  post '/reverse_geocode' do
+    return unless params['lat'].present? or params['lng'].present? # guard statement -- needs an error
+
+    address = @address.geocode(params[:lat], params[:lng])
+
+    location = Location.new(
+      lat: params['lat'],
+      lng: params['lng'],
+      full_address: address
+    )
+
+    if location.save
+      redirect '/'
+    else
+      redirect '/error'
+    end
+  end
+
+  protected
+
+  def parse_full_address(street, city, state, country)
+    [street, city, state, country].compact.join(', ')
+  end
+
+  def new_address
+    @address ||= Address.new
   end
 end
